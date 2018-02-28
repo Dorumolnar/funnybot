4cdf00dd49f9fe3a96f8d758806372d97a2c5aae
Dorumolnar <35419205+Dorumolnar@users.noreply.github.com>
2018-02-28 17:08:06 +0100

    Create Fun.py

---
+++ Fun.py
@@ -0,0 +1,88 @@
+import discord
+from discord.ext import commands
+from random import randint
+from random import choice
+from enum import Enum
+
+class RPS(Enum):
+    rock     = "\N{MOYAI}"
+    paper    = "\N{PAGE FACING UP}"
+    scissors = "\N{BLACK SCISSORS}"
+
+
+class RPSParser:
+    def __init__(self, argument):
+        argument = argument.lower()
+        if argument == "rock":
+            self.choice = RPS.rock
+        elif argument == "paper":
+            self.choice = RPS.paper
+        elif argument == "scissors":
+            self.choice = RPS.scissors
+        else:
+            raise
+
+class Fun:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.stopwatches = {}
+        self.ball = ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good",
+                     "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
+                     "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+    @commands.command(name="8ball")
+    async def _8ball(self, *, question : str):
+        """Ask 8 ball a question"""
+        if question.endswith("?") and question != "?":
+            await self.bot.say("**" + choice(self.ball) + "**")
+        else:
+            await self.bot.say("This isnt a question.")
+
+    @commands.command(pass_context=True)
+    async def stopwatch(self, ctx):
+        """Starts/stops stopwatch"""
+        author = ctx.message.author
+        if not author.id in self.stopwatches:
+            self.stopwatches[author.id] = int(time.perf_counter())
+            await self.bot.say(author.mention + " Stopwatch started!")
+        else:
+            tmp = abs(self.stopwatches[author.id] - int(time.perf_counter()))
+            tmp = str(datetime.timedelta(seconds=tmp))
+            await self.bot.say(author.mention + " Stopwatch stopped! Time: **" + tmp + "**")
+            self.stopwatches.pop(author.id, None)
+
+    @commands.command(pass_context=True)
+    async def rps(self, ctx, your_choice : RPSParser):
+        """Play rock paper scissors"""
+        author = ctx.message.author
+        player_choice = your_choice.choice
+        red_choice = choice((RPS.rock, RPS.paper, RPS.scissors))
+        cond = {
+                (RPS.rock,     RPS.paper)    : False,
+                (RPS.rock,     RPS.scissors) : True,
+                (RPS.paper,    RPS.rock)     : True,
+                (RPS.paper,    RPS.scissors) : False,
+                (RPS.scissors, RPS.rock)     : False,
+                (RPS.scissors, RPS.paper)    : True
+               }
+
+        if red_choice == player_choice:
+            outcome = None # Tie
+        else:
+            outcome = cond[(player_choice, red_choice)]
+
+        if outcome is True:
+            await self.bot.say("{} You win {}!"
+                               "".format(red_choice.value, author.mention))
+        elif outcome is False:
+            await self.bot.say("{} You lose {}!"
+                               "".format(red_choice.value, author.mention))
+        else:
+            await self.bot.say("{} We're square {}!"
+                               "".format(red_choice.value, author.mention))
+
+def setup(bot):
+    bot.add_got(Fun(bot))
+    print("Fun loaded!")
